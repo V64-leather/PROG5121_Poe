c955786470e2a383ff9f65a27c1b3ac5b41b2c10
Oratile <oraextrememoja@gmail.com>
2026-09-19 23:59:37 +0200

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,11 +2,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.prog5121_poe_part1; // Make sure this matches your package name!
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 /**
  *
  * @author oraex
  */
+
 public class LoginTest {
-    
+
+    Login login = new Login();
+
+    @Test
+    public void testUsernameCorrectlyFormatted() {
+        assertTrue(login.checkUserName("kyl_1"));
+    }
+
+    @Test
+    public void testUsernameIncorrectlyFormatted() {
+        assertFalse(login.checkUserName("kyle!!!!!!"));
+    }
+
+    @Test
+    public void testPasswordMeetsComplexityRequirements() {
+        assertTrue(login.checkPasswordComplexity("Ch&&secake99!"));
+    }
+
+    @Test
+    public void testPasswordDoesNotMeetComplexityRequirements() {
+        assertFalse(login.checkPasswordComplexity("password"));
+    }
+
+    @Test
+    public void testCellPhoneCorrectlyFormatted() {
+        assertTrue(login.checkCellPhoneNumber("+27838968976"));
+    }
+
+    @Test
+    public void testCellPhoneIncorrectlyFormatted() {
+        assertFalse(login.checkCellPhoneNumber("08966553"));
+    }
+
+    private void assertTrue() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void assertFalse(boolean checkUserNameboolean checkUserName) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
